59bf9a4c5d66f36bd79186d3153181d248ae39ca
Rob van Geloven <rob@vangeloven.net>
2021-12-01 21:23:03 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,54 @@
+﻿Part1();
+
+Part2();
+
+void Part1()
+{
+    int increases = 0;
+
+    File.ReadAllLines("input1.txt")
+        .Select(x => int.Parse(x))
+        .Aggregate((previous, current) =>
+        {
+            if (current > previous)
+            {
+                increases++;
+            }
+            return current;
+        });
+
+    Console.WriteLine($"Part 1: Depth increased {increases} times.");
+}
+
+void Part2()
+{
+    int increases = 0;
+
+    int windowSize = 3;
+    var items = File.ReadAllLines("input2.txt").Select(x => int.Parse(x)).ToArray();
+
+    for(int index = windowSize + 1; index <= items.Length; index++)
+    {
+        var previousWindowSum = SumWindow(new Span<int>(items, index - windowSize -1, windowSize));
+        var currentWindowSum = SumWindow(new Span<int>(items, index - windowSize, windowSize));
+
+        if(currentWindowSum > previousWindowSum)
+        {
+            increases++;
+        }
+    }
+
+    Console.WriteLine($"Part 2: Depth increased {increases} times.");
+}
+
+int SumWindow(Span<int> window)
+{
+    var result = 0;
+
+    foreach (var item in window)
+    {
+        result += item;
+    }
+
+    return result;
+}
